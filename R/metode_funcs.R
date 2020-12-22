38eded72d30c294eec880540e967a711ebc7b5e3
Jentoft <coo@ssb.no>
2020-12-22 15:51:46 +0100

    Added folder and file for exporting functions

---
+++ R/metode_funcs.R
@@ -0,0 +1,10 @@
+# Functions for inclusion in metodebibr
+
+#' @importFrom CalibrateSSB PanelEstimation
+#' @export
+PanelEstimation <- CalibrateSSB::PanelEstimation
+
+
+#' @importFrom klassR GetKlass
+#' @export
+GetKlass <- klassR::GetKlass
