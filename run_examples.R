2a0fafd28bf2eba0c6ad9103553482e3fc26416a
Jentoft <coo@ssb.no>
2020-12-22 16:08:07 +0100

    Added additional example code

---
+++ run_examples.R
@@ -0,0 +1,15 @@
+# install package (one time only)
+library(devtools)
+install_github("statisticsnorway/metodebibr")
+
+# load library
+library(metodebibr)
+
+# Example of GetKlass function
+regions <- GetKlass(131)
+head(regions)
+
+
+# Example of help file
+?PanelEstimation
+PanelEstimation(2,4)
