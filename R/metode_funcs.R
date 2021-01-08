290e33a077f962bb3738722e108ac22a87e6014a
Langsrud <oyl@ssb.no>
2021-01-08 17:37:40 +0100

    CalibrateSSB functions with tests

--- R/metode_funcs.R
+++ R/metode_funcs.R
@@ -1,10 +1,5 @@
 # Functions for inclusion in metodebibr
 
-#' @importFrom CalibrateSSB PanelEstimation
-#' @export
-PanelEstimation <- CalibrateSSB::PanelEstimation
-
-
 #' @importFrom klassR GetKlass
 #' @export
 GetKlass <- klassR::GetKlass
